17b669aa20217d2fa69748787dc6c2641b7893a1
Pasindu Perera <pasindu@Pasindus-MacBook-Air.local>
2022-04-25 23:01:59 +0530

    First commit

---
+++ docs/precache-manifest.0fc37f119a4a7cdd127ba2eb54260469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45ed06d12438f7569a6b",
+    "url": "/static/css/main.ad3432ce.chunk.css"
+  },
+  {
+    "revision": "45ed06d12438f7569a6b",
+    "url": "/static/js/main.45ed06d1.chunk.js"
+  },
+  {
+    "revision": "9f8e85971acaa6d1d1ac",
+    "url": "/static/css/1.59ec5765.chunk.css"
+  },
+  {
+    "revision": "9f8e85971acaa6d1d1ac",
+    "url": "/static/js/1.9f8e8597.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12673693146db4abeb6f52602316be56",
+    "url": "/index.html"
+  }
+];
